7b94dcf18bda3644d205c0c25c9f5b509bff400b
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-09-21 12:14:25 +0530

    finalised issues carousel

--- easyheals/src/components/Issues.jsx
+++ easyheals/src/components/Issues.jsx
@@ -5,9 +5,10 @@ import Slider from "react-slick";
 
 export const Issues = () => {
 
+  // Custom prev arrow component with custom styling
   const CustomPrevArrow = ({ onClick }) => (
     <div
-      className="custom-arrow prev absolute xl:-left-16 2xl:top-24 xl:top-20"
+      className="custom-arrow prev absolute lg:-left-16 2xl:top-28 xl:top-[5.3em] lg:top-[4em] lg:w-[1.7em] xl:w-[2em]"
       onClick={onClick}
     >
       <img src="./navigation_left.svg"/>
@@ -17,7 +18,7 @@ export const Issues = () => {
   // Custom next arrow component with custom styling
   const CustomNextArrow = ({ onClick }) => (
     <div
-      className="custom-arrow next absolute xl:-right-12 2xl:top-24 xl:top-20"
+      className="custom-arrow next absolute lg:-right-16 2xl:top-28 xl:top-[5.3em] lg:top-[4em] lg:w-[1.7em] xl:w-[2em]"
       onClick={onClick}
     >
        <img src="./navigation_right.svg"/>
@@ -36,16 +37,16 @@ nextArrow: <CustomNextArrow />, // Custom next arrow component
   return (
       <div>
         <Slider {...settings} className="flex mx-20">
-          <div className="gap-x-4"> {/* Add a gap between cards */}
+          <div className="mx-2"> {/* Add a gap between cards */}
             <img src="./issues/acne.svg" className="2xl:w-[26em] xl:w-[22em] lg:w-[17em] cursor-pointer"/>
           </div>
-          <div className="gap-x-4"> {/* Add a gap between cards */}
+          <div className="mx-2"> {/* Add a gap between cards */}
             <img src="./issues/men_health.svg" className="2xl:w-[26em] xl:w-[22em] lg:w-[17em] cursor-pointer"/>
           </div>
-          <div className="gap-x-4"> {/* Add a gap between cards */}
+          <div className="mx-2"> {/* Add a gap between cards */}
             <img src="./issues/acne.svg" className="2xl:w-[26em] xl:w-[22em] lg:w-[17em] cursor-pointer"/>
           </div>
-          <div className="gap-x-4"> {/* Add a gap between cards */}
+          <div className="mx-2"> {/* Add a gap between cards */}
             <img src="./issues/men_health.svg" className="2xl:w-[26em] xl:w-[22em] lg:w-[17em] cursor-pointer"/>
           </div>
         </Slider>
