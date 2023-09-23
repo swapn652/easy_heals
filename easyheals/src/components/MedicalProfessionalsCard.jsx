5f6b0601936c78182c8ed4393cf96783cc9c8ed5
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-09-23 19:06:38 +0530

    Added doctors

--- easyheals/src/components/MedicalProfessionalsCard.jsx
+++ easyheals/src/components/MedicalProfessionalsCard.jsx
@@ -5,11 +5,11 @@ export const MedicalProfessionalsCard = ({ imageSrc, name, specialization }) => 
   const rating = '5.0';
 
   return (
-    <div style={{boxShadow: "0px 2.310344934463501px 4.620689868927002px 0px #6060601A"}} className='h-[17em] w-[14.5em] bg-white flex flex-col items-center relative rounded-xl border-[0.1px] border-text-light'>
-      <img src={imageSrc} className="h-[13em] w-[14.5em] rounded-xl" alt={`Dr. ${name}`} />
-      <div className="mt-2 flex flex-col items-center">
-        <div>{`Dr. ${name}`}</div>
-        <div className="text-[13px]">{specialization}</div>
+    <div style={{boxShadow: "0px 2.310344934463501px 4.620689868927002px 0px #6060601A"}} className='2xl:h-[19em] xl:h-[17em] lg:h-[13.5em] 2xl:w-[14.5em] xl:w-[13em] bg-white flex flex-col items-center relative rounded-xl border-[0.1px] border-text-light'>
+      <img src={imageSrc} className="2xl:h-[15em] xl:h-[13.5em] lg:h-[10.7em] rounded-tl-lg rounded-tr-lg" alt={`Dr. ${name}`} />
+      <div className="mt-2 flex flex-col items-center font-[Poppins] font-[500]">
+        <div className="2xl:text-[15px] xl:text-[13px] lg:text-[11px]">{`Dr. ${name}`}</div>
+        <div className="2xl:text-[13px] xl:text-[11px] lg:text-[9px] text-buttonColor">{specialization}</div>
       </div>
 
       {/* Rating (constant value) */}
