347af6091ef95a235337862e3ab65d8f77b095c7
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-09-23 19:27:20 +0530

    some changes

--- easyheals/src/components/FeaturedServices/FeaturedServicesCard.jsx
+++ easyheals/src/components/FeaturedServices/FeaturedServicesCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const FeaturedServicesCard = () => {
   return (
     <div className="w-[17em] h-[10em] mt-2 bg-white border-2 border-black">
-        yeah boi
+        test card
     </div>
   )
 }
