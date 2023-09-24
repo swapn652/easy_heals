924394ea0280adbb2f852a3594e182d23cf5be4c
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-09-24 16:27:28 +0530

    Lab tests done

--- easyheals/src/components/LabTests.jsx
+++ easyheals/src/components/LabTests.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { LabTestCard } from './LabTestCard';
+import { useAuth } from './AuthContext';
+import axios from 'axios';
 
 export const LabTests = () => {
     const divStyle = {
         backgroundImage: `url('./FeaturedServices_bg.svg')`,
         backgroundSize: 'cover', // You can adjust this based on your image requirements
         backgroundRepeat: 'no-repeat'
+    };
+
+    const extractAddress = (address) => {
+        const addressTerms = address.split(',').map((term) => term.trim());
+        if (addressTerms.length >= 3) {
+          const secondTerm = addressTerms[1];
+          const thirdLastTerm = addressTerms[addressTerms.length - 3];
+          const mergedAddress = `${secondTerm}, ${thirdLastTerm}`;
+          if (mergedAddress.length > 25) {
+            return thirdLastTerm;
+          } else {
+            return mergedAddress;
+          }
+        }
+        return address;
       };
+
+    const bearerToken = useAuth();
+  const [labTests, setLabTests] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = 'https://api.development.easyheals.com/Search/featuredDiagnostics';
+
+    axios
+      .get(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${bearerToken}`,
+        },
+      })
+      .then((response) => {
+        const data = response.data.data;
+
+        const labTestInfo = data.map((labTest) => ({
+          title: labTest.title,
+          preparation:
+            labTest.fasting_required === 'No'
+              ? 'No Special Preparation Required'
+              : `At least ${labTest.fasting_hours} hours fasting required`,
+          hospitalName1: labTest.available_labs[0]?.title || '',
+          hospitalAddress1: extractAddress(labTest.available_labs[0]?.address || ''),
+          hospitalPrice1: labTest.available_labs[0]?.price || '',
+          hospitalOfferPrice1: labTest.available_labs[0]?.offer_price || '',
+          hospitalName2: labTest.available_labs[1]?.title || '',
+          hospitalAddress2: extractAddress(labTest.available_labs[1]?.address || ''),
+          hospitalPrice2: labTest.available_labs[1]?.price || '',
+          hospitalOfferPrice2: labTest.available_labs[1]?.offer_price || '',
+        }));
+
+        setLabTests(labTestInfo.slice(0, 3));
+      })
+      .catch((error) => {
+        console.error('Error fetching lab tests:', error);
+      });
+  }, [bearerToken]);
+
 
   return (
     <div className="relative w-screen 2xl:h-[44em] xl:h-[42em] lg:h-[36em] flex flex-row justify-center font-[Raleway]">
@@ -20,10 +77,10 @@ export const LabTests = () => {
             <span className="2xl:w-[540px] xl:w-[500px] lg:w-[380px] h-[1px] bg-text-light"></span>
         </div>
         <div style={divStyle} className="absolute w-screen 2xl:h-[27em] xl:h-[25em] lg:h-[21em] flex flex-row justify-center xl:top-[17em] lg:top-[15em]">
-            <div className="absolute flex flex-row top-12">
-                <img src="./LabTestCard.svg" className="2xl:w-[27em] xl:w-[24em] lg:w-[19em]"/>
-                <img src="./LabTestCard.svg" className="2xl:w-[27em] xl:w-[24em] lg:w-[19em]"/>
-                <img src="./LabTestCard.svg" className="2xl:w-[27em] xl:w-[24em] lg:w-[19em]"/>
+            <div className="absolute flex flex-row top-12 gap-x-4">
+            {labTests.map((labTest, index) => (
+        <LabTestCard key={index} {...labTest} />
+      ))}
             </div>
 
             <div className="
