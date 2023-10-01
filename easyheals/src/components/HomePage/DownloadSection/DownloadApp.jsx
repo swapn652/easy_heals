558cf7982967537d9bda474d472ec24b95ad70c4
SWAPNIL PANT <70851202+swapn652@users.noreply.github.com>
2023-10-01 21:37:48 +0530

    did something

--- easyheals/src/components/HomePage/DownloadSection/DownloadApp.jsx
+++ easyheals/src/components/HomePage/DownloadSection/DownloadApp.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export const DownloadApp = () => {
   return (
-    <div style={{background: "var(--icon-green, linear-gradient(134deg, #74B641 7.31%, #159939 78.04%))"}} className="relative w-screen 2xl:h-[26em] xl:h-[24em] lg:h-[20em] flex flex-row font-[Poppins] xl:mb-[8em] lg:mb-[7em]">
+    <div style={{background: "var(--icon-green, linear-gradient(134deg, #74B641 7.31%, #159939 78.04%))"}} className="relative w-screen 4xl:h-[32em] 2xl:h-[26em] xl:h-[24em] lg:h-[20em] flex flex-row font-[Poppins] xl:mb-[8em] lg:mb-[7em]">
       <div className="flex flex-col absolute text-white 2xl:left-[14em] xl:left-[12em] lg:left-[8em] 2xl:top-[6em] xl:top-[5em] lg:top-[4em]">
-        <div className="2xl:text-[36px] xl:text-[32px] lg:text-[25px] font-[700]">
+        <div className="4xl:text-[52px] 2xl:text-[36px] xl:text-[32px] lg:text-[25px] font-[700]">
           Download the EasyHeals
         </div>
-        <div className="2xl:text-[29px] xl:text-[26px] lg:text-[20px] font-[700]">
+        <div className="4xl:text-[44px] 2xl:text-[29px] xl:text-[26px] lg:text-[20px] font-[700]">
           Mobile Application
         </div>
-        <div className="2xl:text-[13px] xl:text-[12px] lg:text-[10px] mt-4">
+        <div className="4xl:text-[25px] 2xl:text-[13px] xl:text-[12px] lg:text-[10px] mt-4">
           <div>
             Say goodbye to long waiting times and endless
           </div>
@@ -19,12 +19,12 @@ export const DownloadApp = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row gap-x-4 absolute 2xl:left-[14em] xl:left-[12em] lg:left-[8em] 2xl:top-[17em] xl:top-[15em] lg:top-[12em]">
-        <a href="https://play.google.com/store/apps/details?id=com.Easyheals.patient" target="_blank"><img src="./downloads/google_play_store.svg" className="2xl:w-[11em] xl:w-[9em] lg:w-[7.5em] cursor-pointer"/></a>
-        <a href="https://apps.apple.com/app/easyheals/id6462711867" target="_blank"><img src="./downloads/app_store.svg" className="2xl:w-[10em] xl:w-[8em] lg:w-[7em] cursor-pointer"/></a>
+      <div className="flex flex-row gap-x-4 absolute 2xl:left-[14em] xl:left-[12em] lg:left-[8em] 4xl:top-[23em] 2xl:top-[17em] xl:top-[15em] lg:top-[12em]">
+        <a href="https://play.google.com/store/apps/details?id=com.Easyheals.patient" target="_blank"><img src="./downloads/google_play_store.svg" className="4xl:w-[18em] 2xl:w-[11em] xl:w-[9em] lg:w-[7.5em] cursor-pointer"/></a>
+        <a href="https://apps.apple.com/app/easyheals/id6462711867" target="_blank"><img src="./downloads/app_store.svg" className="4xl:w-[17em] 2xl:w-[10em] xl:w-[8em] lg:w-[7em] cursor-pointer"/></a>
       </div>
-      <img src="./downloads/phone_1.svg" className="absolute 2xl:w-[13em] xl:w-[12em] lg:w-[11em] 2xl:right-[25.7em] xl:right-[24em] lg:right-[20em] -top-[3em]"/>
-      <img src="./downloads/phone_2.svg" className="absolute 2xl:w-[30em] xl:w-[28em] lg:w-[23.5em] right-0 xl:-top-[6em] lg:-top-[5em]"/>
+      <img src="./downloads/phone_1.svg" className="absolute 4xl:w-[18em] 2xl:w-[13em] xl:w-[12em] lg:w-[11em] 4xl:right-[29em] 2xl:right-[25.7em] xl:right-[24em] lg:right-[20em] 4xl:-top-[5em] -top-[3em]"/>
+      <img src="./downloads/phone_2.svg" className="absolute 4xl:w-[38em] 2xl:w-[30em] xl:w-[28em] lg:w-[23.5em] right-0 4xl:-top-[8em] xl:-top-[6em] lg:-top-[5em]"/>
       <img src="./plus_bg_5.svg" className="2xl:w-[15em] xl:w-[13em] lg:w-[12em] absolute right-0 bottom-0"/>
     </div>
   )
--- easyheals/src/components/HomePage/Footers/Footer.jsx
+++ easyheals/src/components/HomePage/Footers/Footer.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className=" w-screen relative">
-        <div className='absolute 2xl:h-[22em] xl:h-[21em] lg:h-[20em] w-screen bg-footer-color flex flex-row'>
+        <div className='absolute 4xl:h-[30em] 2xl:h-[22em] xl:h-[21em] lg:h-[20em] w-screen bg-footer-color flex flex-row'>
             {/* Logo and social media links */}
             <div className="flex flex-col">
                 <img src="./easyHealsLogo.svg" alt="Easy heals logo" className="absolute top-[4em] left-[5em] 2xl:w-[16em] xl:w-[13em] lg:w-[10em]"/>
                 <div className="font-[Raleway] 2xl:text-[18px] xl:text-[15px] lg:text-[12px] italic absolute 2xl:left-[8.5em] xl:left-[9em] lg:left-[10em] 2xl:top-[6.8em] xl:top-[7em] lg:top-[8em]">Easy access to Healthcare</div>
                 <div className="flex flex-row xl:gap-x-4 lg:gap-x-2 absolute 2xl:top-[11.5em] xl:top-[10em] lg:top-[9em] left-[5em]">
-                    <a href="https://www.facebook.com/easyheals" target="_blank"><img src="./social_media/facebook.svg" alt="facebook" className="2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
-                    <a href="https://www.instagram.com/easy_heals/" target="_blank"><img src="./social_media/instagram.svg" alt="instagram" className="2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
-                    <a href="https://www.linkedin.com/company/easyheals" target="_blank"><img src="./social_media/linkedin.svg" alt="linkedin" className="2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
-                    <a href="https://www.youtube.com/@easyheals" target="_blank"><img src="./social_media/youtube.svg" alt="youtube" className="2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
-                    <a href="https://twitter.com/easyheals" target="_blank"><img src="./social_media/twitter.svg" alt="twitter" className="2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
+                    <a href="https://www.facebook.com/easyheals" target="_blank"><img src="./social_media/facebook.svg" alt="facebook" className="4xl:w-[3em] 2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
+                    <a href="https://www.instagram.com/easy_heals/" target="_blank"><img src="./social_media/instagram.svg" alt="instagram" className="4xl:w-[3em] 2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
+                    <a href="https://www.linkedin.com/company/easyheals" target="_blank"><img src="./social_media/linkedin.svg" alt="linkedin" className="4xl:w-[3em] 2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
+                    <a href="https://www.youtube.com/@easyheals" target="_blank"><img src="./social_media/youtube.svg" alt="youtube" className="4xl:w-[3em] 2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
+                    <a href="https://twitter.com/easyheals" target="_blank"><img src="./social_media/twitter.svg" alt="twitter" className="4xl:w-[3em] 2xl:w-[2.4em] xl:w-[2em] lg:w-[1.8em] cursor-pointer"/></a>
                 </div>
             </div>
 
             {/* Company */}
-            <div className="flex flex-col font-[Raleway] 3xl:text-[20px] 2xl:text-[16px] xl:text-[15px] lg:text-[13px] gap-y-4 absolute 3xl:top-[3em] xl:top-[5em] lg:top-[5.5em 3xl:left-[25em] 2xl:left-[32em] xl:left-[30em] lg:left-[28em]">
+            <div className="flex flex-col font-[Raleway] 4xl:text-[27px] 3xl:text-[20px] 2xl:text-[16px] xl:text-[15px] lg:text-[13px] gap-y-4 absolute 4xl:top-[2.2em] 3xl:top-[3em] xl:top-[5em] lg:top-[5.5em] 4xl:left-[19em] 3xl:left-[25em] 2xl:left-[32em] xl:left-[30em] lg:left-[28em]">
                 <div className="font-[600] cursor-pointer">Company</div>
                 <div className="cursor-pointer">About Us</div>
                 <div className="cursor-pointer">Terms & Conditions</div>
@@ -27,7 +27,7 @@ export const Footer = () => {
             </div>
             
             {/* Services */}
-            <div className="flex flex-col font-[Raleway] 3xl:text-[20px] 2xl:text-[16px] xl:text-[15px] lg:text-[13px] gap-y-4 absolute 3xl:top-[3em] xl:top-[5em] lg:top-[5.5em] 3xl:left-[40em] 2xl:left-[50em] xl:left-[47em] lg:left-[44em]">
+            <div className="flex flex-col font-[Raleway] 4xl:text-[27px] 3xl:text-[20px] 2xl:text-[16px] xl:text-[15px] lg:text-[13px] gap-y-4 absolute 4xl:top-[2.2em] 3xl:top-[3em] xl:top-[5em] lg:top-[5.5em] 4xl:left-[34em] 3xl:left-[40em] 2xl:left-[50em] xl:left-[47em] lg:left-[44em]">
                 <div className="font-[600] cursor-pointer">Services</div>
                 <div className="cursor-pointer">Treatment</div>
                 <div className="cursor-pointer">Doctor Appointment</div>
@@ -36,7 +36,7 @@ export const Footer = () => {
             </div>
 
             {/* Contact info */}
-            <div className="flex flex-col font-[Raleway] 3xl:text-[20px] 2xl:text-[16px] xl:text-[15px] lg:text-[13px] gap-y-6 absolute 3xl:top-[3em] xl:top-[5em] lg:top-[5.5em] 3xl:left-[56em] 2xl:left-[67em] xl:left-[64em] lg:left-[60em]">
+            <div className="flex flex-col font-[Raleway] 4xl:text-[27px] 3xl:text-[20px] 2xl:text-[16px] xl:text-[15px] lg:text-[13px] gap-y-6 absolute 4xl:top-[2.2em] 3xl:top-[3em] xl:top-[5em] lg:top-[5.5em] 4xl:left-[50em] 3xl:left-[56em] 2xl:left-[67em] xl:left-[64em] lg:left-[60em]">
                 <div className="flex flex-row">
                     <img src="./contact_info/phone.svg" className="w-[1.5em]"/>
                     <span className="ml-2 font-[600]">+91-7510818108</span>
@@ -52,14 +52,14 @@ export const Footer = () => {
                 <div className="font-[600] text-[#678765]">
                     Download Mobile Application
                 </div>
-                <div className="flex flex-row gap-x-4 absolute 3xl:top-[10.5em] lg:top-[13em] xl:top-[12em]">
+                <div className="flex flex-row gap-x-4 absolute 4xl:top-[10em] 3xl:top-[10.5em] lg:top-[13em] xl:top-[12em]">
                     <a href="https://play.google.com/store/apps/details?id=com.Easyheals.patient" target="_blank"><img src="./downloads/google_play_store.svg" className="cursor-pointer xl:w-[9em] lg:w-[8em]"/></a>
                     <a href="https://apps.apple.com/app/easyheals/id6462711867" target="_blank"><img src="./downloads/app_store.svg" className="cursor-pointer xl:w-[8em] lg:w-[7em]"/></a>
                 </div>
             </div>
         </div>
 
-        <div className="bg-green2 h-[4em] w-screen absolute 2xl:top-[22em] xl:top-[21em] lg:top-[25em] flex items-center justify-center text-white xl:text-base lg:text-[12px] font-[Raleway] font-[600]">
+        <div className="bg-green2 h-[4em] w-screen absolute 4xl:top-[27em] 2xl:top-[22em] xl:top-[21em] lg:top-[25em] flex items-center justify-center text-white xl:text-base lg:text-[12px] font-[Raleway] font-[600]">
             Copyright © 2023 by EasyHeals Technologies Private Limited. All Rights Reserved.
         </div>
     </div>
